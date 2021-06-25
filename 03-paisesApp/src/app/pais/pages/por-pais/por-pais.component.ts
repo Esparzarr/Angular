077fc7ff4d6ae84fef10f95779340d302aa76a8b
Esparzarr <61842004+Esparzarr@users.noreply.github.com>
2021-06-25 16:45:22 -0500

    UPDATE PAIS

--- 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais-interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -9,12 +11,28 @@ import { Component } from '@angular/core';
 export class PorPaisComponent {
 
   termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
+  
 
-  constructor() { }
+  constructor( private paisService: PaisService) { }
 
 
-  buscar(){
-    console.log(this.termino);
+  buscar( termino: string){
+    this.hayError = false;
+    this.termino = termino;
+
+    this.paisService.buscarPais(termino).subscribe( (paises) => {
+      this.paises = paises
+    }, (err) => {
+      this.hayError = true;
+      this.paises = [];
+    });
+  }
+
+  sugerencias( termino: string){
+    this.hayError = false
+
   }
 
 }
