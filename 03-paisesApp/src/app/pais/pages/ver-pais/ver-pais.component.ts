bb7645cbbe14b7127e1977f81baa6f2abc8138b0
angel <aesparza@ucol.mx>
2021-06-26 22:42:59 -0500

    UUPDATE FINIST APP PAISES

--- 03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators'
+
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais-interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -10,21 +13,32 @@ import { PaisService } from '../../services/pais.service';
 })
 export class VerPaisComponent implements OnInit {
 
-  constructor( 
+  pais!: Country;
+
+  constructor(
     private activatedRoute: ActivatedRoute,
     private paisService: PaisService
-    ) { }
+  ) { }
 
   ngOnInit(): void {
-    this.activatedRoute.params
-      .subscribe( ({id}) => {
-        console.log(id)
 
-        this.paisService.getPaisId(id)
-          .subscribe(pais => {
-            console.log(pais)
-          })
-      })
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => this.paisService.getPaisId(id)),
+        tap(console.log)
+      )
+      .subscribe(res => this.pais = res)
+
+
+    // this.activatedRoute.params
+    //   .subscribe( ({id}) => {
+    //     console.log(id)
+
+    //     this.paisService.getPaisId(id)
+    //       .subscribe(pais => {
+    //         console.log(pais)
+    //       })
+    //   })
   }
 
 }
